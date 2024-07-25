f69737a66b06e6d2b65bdf838d150702a0bf410c
tangxuelong <2753413812@qq.com>
2024-07-25 11:47:39 +0800

    feat:
    Signed-off-by: tangxuelong <2753413812@qq.com>

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -28,6 +28,10 @@ export default class index extends Component {
                 <Form.Check.Label>{`已完成 ${doneCount}/全部 ${totalCount}`}</Form.Check.Label>
             </div>
             <Button variant="danger" size="sm" onClick={this.handleClearDone}>清除已完成任务</Button>
+            <a className="footer-icp" href='https://beian.mps.gov.cn/#/query/webSearch'>
+                <img src="https://beian.mps.gov.cn/img/logo01.dd7ff50e.png" alt="" />
+                <span>京ICP备2024078215号</span>
+            </a>
         </div>
     )
   }
